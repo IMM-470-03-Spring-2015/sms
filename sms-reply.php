b944872543a9331a29bb8000a37899310e1ca580
Mark D. Thompson <thomesoni@gmail.com>
2015-01-16 13:03:37 -0500

    Update sms-reply.php

--- sms-reply.php
+++ sms-reply.php
@@ -2,8 +2,8 @@
  
     // make an associative array of senders we know, indexed by phone number
     $people = array(
-        "+12152642459" => "Curious George",
-        "+2678646825" => "Sharleen",
+        "+12121234567" => "Curious George",
+        "+2123217654" => "Mom",
     );
  
     // if the sender is known, then greet them by name
